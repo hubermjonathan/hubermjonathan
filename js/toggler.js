585924da4fdbd9b1c4e930b4cfb8ce2cb581e4df
Jon Huber <hubermjonathan@gmail.com>
2019-09-10 22:16:14 -0400

    new simple design

--- js/toggler.js
+++ js/toggler.js
@@ -1,40 +1,16 @@
-let landing = document.getElementById("landing");
-let projects = document.getElementById("projects");
-
-document.getElementById("go-to-projects").addEventListener("click", () => {
-  landing.classList.add("hidden");
-  projects.classList.remove("hidden");
-});
-
-document.getElementById("go-to-landing").addEventListener("click", () => {
-  projects.classList.add("hidden");
-  landing.classList.remove("hidden");
-});
-
 document.getElementById("theme-toggler").addEventListener("click", () => {
-  console.log(getComputedStyle(document.documentElement).getPropertyValue("--background-color"));
-  if (getComputedStyle(document.documentElement).getPropertyValue("--background-color") === "#fff" || getComputedStyle(document.documentElement).getPropertyValue("--background-color") === " #fff") {
-    document.documentElement.style.setProperty("--background-color", "#000");
-    document.documentElement.style.setProperty("--text-1", "#fff");
-    document.documentElement.style.setProperty("--text-2", "#ccc");
-
-    let cards = Array.from(document.getElementsByClassName("card"));
-    for (let i = 0; i < cards.length; i++) {
-      cards[i].classList.add("bg-dark");
-    }
+  if (getComputedStyle(document.documentElement).getPropertyValue("--background") === "#fff" || getComputedStyle(document.documentElement).getPropertyValue("--background") === " #fff") {
+    document.documentElement.style.setProperty("--background", "#000");
+    document.documentElement.style.setProperty("--text", "#fff");
+    document.documentElement.style.setProperty("--text-hover", "#ccc");
 
     let icon = document.getElementById("theme-toggler");
     icon.classList.add("fas");
     icon.classList.remove("far");
   } else {
-    document.documentElement.style.setProperty("--background-color", "#fff");
-    document.documentElement.style.setProperty("--text-1", "#000");
-    document.documentElement.style.setProperty("--text-2", "#333");
-
-    let cards = Array.from(document.getElementsByClassName("card"));
-    for (let i = 0; i < cards.length; i++) {
-      cards[i].classList.remove("bg-dark");
-    }
+    document.documentElement.style.setProperty("--background", "#fff");
+    document.documentElement.style.setProperty("--text", "#000");
+    document.documentElement.style.setProperty("--text-hover", "#333");
 
     let icon = document.getElementById("theme-toggler");
     icon.classList.add("far");
